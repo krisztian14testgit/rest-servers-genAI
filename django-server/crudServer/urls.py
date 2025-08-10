6e3618727492444e5ce37de64b9a7966f6950a1d
krisztian14testgit <krisztian14.s@outlook.com>
2025-08-10 19:00:56 +0200

    changed route alias name at element:id

--- django-server/crudServer/urls.py
+++ django-server/crudServer/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("elements", views.get_post_elements_handler, name="requests_elementS"),
-    path("element/<int:id>", views.get_put_delete_element_handler, name="requests_element"),
+    path("element/<int:id>", views.get_put_delete_element_handler, name="requests_byIds"),
 ]
